2a81cc53e53bb4d15f6c60ad1954570c66dc5c04
Alexander <alexander.niehaus@gmx.net>
2024-05-13 12:20:40 +0200

    finish ticket 2

---
+++ components/Spotlight.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ArtPiecesPreview from "./ArtPiecesPreview";
+
+const Spotlight = ({ image }) => {
+  return (
+    <>
+      <ArtPiecesPreview image={image} />
+    </>
+  );
+};
+
+export default Spotlight;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import useSWR from "swr";
 import Image from "next/image";
 import ArtPieces from "@/components/ArtPieces";
+import Spotlight from "@/components/Spotlight";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
@@ -13,13 +14,18 @@ export default function HomePage() {
   if (error) return <div>failed to load</div>;
   if (isLoading) return <div>loading...</div>;
 
-  console.log(data);
+  const randomPiece = Math.random() * (11 - 1) + 1;
+
+  const roundedRandomPiece = Math.round(randomPiece);
 
   // render data
 
   return (
     <div>
       <h1>Art Gallery</h1>
+      <h2>Spotlight</h2>
+      <Spotlight image={data[roundedRandomPiece]} />
+      <h2>Pieces</h2>
       <ArtPieces pieces={data} />
     </div>
   );
