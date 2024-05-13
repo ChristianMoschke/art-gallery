78b0c5cd97f753245ee8456346f0f15ececd757c
Alexander <alexander.niehaus@gmx.net>
2024-05-13 17:48:24 +0200

    finish ticket 5

--- pages/_app.js
+++ pages/_app.js
@@ -6,11 +6,27 @@ import { useDeferredValue, useEffect, useState } from "react";
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function App({ Component, pageProps }) {
-  const [artPiecesInfo, setArtPiecesInfo] = useState();
+  const [artPiecesInfo, setArtPiecesInfo] = useState([]);
 
-  function handleFavorites() {
-    return;
+  function handleFavorites(pieceName) {
+    const existingIndex = artPiecesInfo.findIndex(
+      (item) => item.name === pieceName
+    );
+    if (existingIndex !== -1) {
+      const updatedArtPiecesInfo = [...artPiecesInfo];
+      updatedArtPiecesInfo[existingIndex] = {
+        ...updatedArtPiecesInfo[existingIndex],
+        isFavorite: !updatedArtPiecesInfo[existingIndex].isFavorite,
+      };
+      setArtPiecesInfo(updatedArtPiecesInfo);
+    } else {
+      setArtPiecesInfo((prevState) => [
+        ...prevState,
+        { name: pieceName, isFavorite: true },
+      ]);
+    }
   }
+  console.log(artPiecesInfo);
 
   const { data, error, isLoading } = useSWR(
     "https://example-apis.vercel.app/api/art",
