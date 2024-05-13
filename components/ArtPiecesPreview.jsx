ac9113f8f34c68495a8c70f2fb065508a9daae6a
Alexander <alexander.niehaus@gmx.net>
2024-05-13 15:29:02 +0200

    finish ticket 4

--- components/ArtPiecesPreview.jsx
+++ components/ArtPiecesPreview.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const ArtPiecesPreview = ({ image }) => {
   return (
     <li>
-      <Image
-        src={image.imageSource}
-        height={244}
-        width={244}
-        alt="a picture of art"
-      />
+      <Link href={`/art-pieces/${image.slug}`}>
+        <Image
+          src={image.imageSource}
+          height={244}
+          width={244}
+          alt="a picture of art"
+        />
+      </Link>
       <p>{image.artist}</p>
       <p>{image.name}</p>
     </li>
